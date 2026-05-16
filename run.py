e039bd38187d64f5b66b8dd03d8e5ede0dda7268
esther juma <jumanelima77@gmail.com>
2026-05-16 10:46:53 +0300

    Imported models for database creation

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from app import create_app
-from app import db
+from app import create_app, db
+from app.models import User, Complaint
 app = create_app()
 with app.app_context():
     db.create_all()
